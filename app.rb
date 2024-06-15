67ad3bcc3ba82a0fcdf5b31df13227eb52625991
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-06-16 00:49:31 +0900

    update

--- app.rb
+++ app.rb
@@ -1,44 +1,45 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def flist_to_h(cksum_file_name)
+def flist_to_h(cksum_file_name, prefix = '')
   hash = {}
   lines = File.readlines(cksum_file_name).map(&:chomp)
   lines.map do |line|
     parts = line.match(/^(\d+)\s+\d+\s+(.*)$/)
     sum_value = parts[1].to_i
-    file_name = parts[2]
+    file_name = prefix + parts[2].gsub(%r{^\./}, '')
     hash[file_name] = sum_value
   end
   hash
 end
 
 # change?
-def extract_change(list_a, list_b, prefix = '', _suffix = '')
+def extract_change(list_a, list_b)
   diff = []
-
   list_b.each do |target_file_name, fsum|
-    target_file_name = prefix + target_file_name
-
     diff << target_file_name if list_a.key?(target_file_name) && fsum != list_a[target_file_name]
   end
   diff
 end
 
-# del?
-def delete_list(list_a, list_b, _prefix = '')
-  a_keys = list_a.keys
-  b_keys = list_b.keys
-  b_keys - a_keys
+# add?
+def add_list(list_a, list_b)
+  list_b.keys - list_a.keys
 end
 
 if __FILE__ == $PROGRAM_NAME
-  a = flist_to_h(ARGV.first)
-  b = flist_to_h(ARGV.last)
+  a = flist_to_h(ARGV.first) # old
+  b = flist_to_h(ARGV.last) # new
 
   ec = extract_change(a, b)
+  puts 'CHANGE:'
   p ec
 
-  dl = delete_list(a, b)
+  puts 'ADD:'
+  al = add_list(a, b)
+  p al
+
+  puts 'DELETE:'
+  dl = add_list(b, a)
   p dl
 end
